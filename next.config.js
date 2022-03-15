b102c007191ddda6dc92f8f2577c756b65c0d710
Abhishek Pednekar <abhi_ap@hotmail.com>
2022-03-15 17:53:13 +0530

    Updated the CSP for analytics

--- next.config.js
+++ next.config.js
@@ -11,9 +11,9 @@ module.exports = {
             key: "Content-Security-Policy",
             value: `default-src * ${
               process.env.NODE_ENV === "development" && "data: 'unsafe-eval'"
-            }; script-src 'self' ${
+            }; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com ${
               process.env.NODE_ENV === "development" && "data: 'unsafe-eval'"
-            }; img-src * data: blob:; style-src * data: 'unsafe-inline'; font-src 'self' https://fonts.gstatic.com data: 'unsafe-inline'; object-src 'self' blob:`,
+            }; img-src * www.googletagmanager.com data: blob:; style-src * data: 'unsafe-inline'; font-src 'self' https://fonts.gstatic.com data: 'unsafe-inline'; object-src 'self' blob:`,
           },
           {
             key: "X-DNS-Prefetch-Control",
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import smoothscroll from "smoothscroll-polyfill";
 import { motion } from "framer-motion";
 import RecipeState from "@/context/RecipeState";
+import Script from "next/script";
 
 const pageVariants = {
   pageInitial: {
@@ -27,6 +28,21 @@ function MyApp({ Component, pageProps, router }) {
       initial="pageInitial"
       animate="pageAnimate"
     >
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+      />
+      <Script id="ga-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+        `}
+      </Script>
       <RecipeState>
         <Component {...pageProps} />
       </RecipeState>

a50166bab0ff704790deb9362ba1d53c17959c14
Abhishek Pednekar <abhi_ap@hotmail.com>
2022-05-10 10:58:20 +0530

    Google Tag Manager integration

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer',${process.env.NEXT_PUBLIC_GTM_ID}`,
+            }}
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500&family=Poppins:ital,wght@0,400;0,600;0,700;0,800;0,900;1,400&display=swap"
             rel="stylesheet"
@@ -30,6 +40,12 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID}"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
         </body>
       </Html>
     );
